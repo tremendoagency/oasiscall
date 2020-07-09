01d6ab12deb963c41942012c4451ebc6c7019d32
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-07-09 17:10:37 -0300

    v3.1

--- js/magic.js
+++ js/magic.js
@@ -44,70 +44,67 @@
 
   // Navigation
 
-  if ($('body').hasClass('home')){
+  var navigation = new Swiper('#navigation', {
+    speed: 1000,
+    direction: 'vertical',
+    mousewheel: true,
+    forceToAxis: true,
+    hashNavigation:true
+  });
 
-    var navigation = new Swiper('#navigation', {
-      speed: 1000,
-      direction: 'vertical',
-      mousewheel: true,
-      forceToAxis: true,
-      hashNavigation:true
-    });
+  navigation.on('slideChangeTransitionStart', function () {
+    invertNavbar();
+  });
 
-    navigation.on('slideChangeTransitionStart', function () {
-      invertNavbar();
-    });
-
-    function invertNavbar(){
-      if ($('.swiper-slide-active').hasClass('invert-menu')){
-        $('nav').addClass('inverted');
-      }
-      else{
-          $('nav').removeClass('inverted');
-      }
+  function invertNavbar(){
+    if ($('.swiper-slide-active').hasClass('invert-menu')){
+      $('nav').addClass('inverted');
     }
-
-    //Team
-    
-    var team = new Swiper('#team .swiper-container', {
-      speed: 1000,
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 1,
-      spaceBetween: 0,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 0,
-        },
-        992: {
-          slidesPerView: 3,
-          spaceBetween: 0,
-        },
-        1200: {
-          slidesPerView: 4,
-          spaceBetween: 0,
-        },
-      }
-    });
-
-    $('#navbar-toggler').click(function() {
-      $('#navbar').toggleClass('open');
-      $('.mobile-navbar').toggleClass('show');
-    });
-  
-    $('.slide-to').on( "click", function(event){
-      event.preventDefault();
-      navigation.slideTo($(this).attr('data-slide'));
-      $('.mobile-navbar').removeClass('show');
-      $('#navbar').removeClass('open');
-    });
-
+    else{
+        $('nav').removeClass('inverted');
+    }
   }
+
+  //Team
+
+  var team = new Swiper('#team .swiper-container', {
+    speed: 1000,
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+    }
+  });
+
+  $('#navbar-toggler').click(function() {
+    $('#navbar').toggleClass('open');
+    $('.mobile-navbar').toggleClass('show');
+  });
+
+  $('.slide-to').on( "click", function(event){
+    event.preventDefault();
+    navigation.slideTo($(this).attr('data-slide'));
+    $('.mobile-navbar').removeClass('show');
+    $('#navbar').removeClass('open');
+  });
+
 
   // MailChimp Form
 
